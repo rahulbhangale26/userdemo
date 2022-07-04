2ee5f4a3e8599bc69fbcab5663c69feec07fa98b
rbhangale <rbhangale@xento.com>
2022-07-04 18:34:45 +0530

    Setting up database

--- userdemo/settings.py
+++ userdemo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'userdemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',
+        'USER': '****',
+        'PASSWORD': '****',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
